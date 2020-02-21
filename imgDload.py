545cf7b8a86ae8f123b4423eb5b4ebf7c6bb9ad2
johnnietien <johnnietien@gmail.com>
2020-02-22 06:14:30 +0700

    Add files via upload

--- imgDload.py
+++ imgDload.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
-
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+from pandas import DataFrame
 import urllib.request as urllib2
-import os
+import os, time
 
-default_dir = ".../imgsnew"
-
-df = pd.read_csv(r'.../picsnew.csv')
+default_dir = "./imgsnew"
+df = pd.read_csv(r'testlinkcsv.csv')
+#df = pd.read_csv(r'testlinkcsv5.csv', nrows=3)
+#df = df.head(3)
 os.chdir(default_dir)
 
 titles = df['titles']
@@ -20,17 +26,46 @@ options.add_experimental_option("prefs", {
   "safebrowsing.enabled": True
 })
 
-driver = webdriver.Chrome(".../chromedriver", options=options)
+driver = webdriver.Chrome("D:\Admin\Projects2020\ImageDownloader\chromedriver_win32\chromedriver.exe", options=options)
+#driver.maximize_window()
+driver.implicitly_wait(10) # seconds
 
 for idx, event in enumerate(links):
     driver.get(event)
+    productTitle = driver.find_element_by_xpath('//*[@id="productTitle"]')
+    print(productTitle.text)
+    #df['titles'] = productTitle.text
+    #print(df['titles'])
+    titles[idx] = productTitle.text
+    #print(titles[idx])
+    #df.iloc[idx,0] = productTitle.text
+    #image_path = driver.find_element_by_xpath('//*[@id="imageBlock"]/div/div/div[2]')
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main-image-container"]')))
+    print('da doi xong')
 
-    image_path = driver.find_element_by_xpath('//img')
-    src = image_path.get_attribute('src')
+    actions = ActionChains(driver)
+    #actions.move_to_element(element)
+    actions.context_click(element).perform()
+    #actions.perform()
+    #element.click()
+    print('da click xong')
 
-    # download the image
-    urllib2.urlretrieve(src, titles[idx] + ".jpg")
+    wait = WebDriverWait(driver, 20)
+    image_path = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="landingImage"]')))
+    src = image_path.get_attribute('data-old-hires')
+    print(image_path)
+    # download the image //*[@id="main-image-container"]/ul/li[3]/span/span/div/img
+    # //*[@id="ask-dp-search_feature_div"]/div/div/div/div/form/span/span/span/span/span/span/div/span/div/div/div/input
+    # urllib2.urlretrieve(src, titles[idx] + ".jpg")
+    #urllib2.urlretrieve(src, productTitle.text + ".png")
+    #driver.close()
+    urllib2.urlretrieve(src, productTitle.text + ".png")
 
+df['titles'] = titles
+print(df['titles'])
+df1 = DataFrame(df, columns= ['titles', 'links'])
+export_csv = df1.to_csv(r'testlinkcsv5.csv', index = None, header=True)
 driver.close()
 
 
